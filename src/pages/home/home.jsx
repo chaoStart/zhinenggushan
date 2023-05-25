294129e1e06cdddb8c5cf5b6f69f862c67ff2d64
chaoStart <1416860952@qq.com>
2023-05-25 11:31:16 +0800

    在本机正常运行的react项目

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,7 +6,7 @@ Home 路由组件
 export default function Home(props) {
     return (
         <div className="home">
-            欢迎使用609所智能制造生产过程管理系统
+            欢迎使用609所智能制造生产过程订单管理系统
         </div>
     )
 }
